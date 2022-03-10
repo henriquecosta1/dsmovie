ebf67ea3b8eeb76dd8dc347751874432ea76653d
FABIO HENRIQUE CRISTO COSTA <02141087244@TRT8.net>
2022-03-10 18:55:36 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,10 +1,36 @@
 import Pagination from "components/Pagination";
-import "./styles.css"
+import MovieCard from "components/MovieCard";
+import './styles.css'
 
 function Listing() {
 
     return (
-        <Pagination />
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
